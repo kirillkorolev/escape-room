71cb4e031db018732e611e488d983361f5639f1c
Кирилл <kkirillcore@gmail.com>
2019-08-24 20:14:31 +0300

    сделал меню фильр с выбором квестов, сетка квестов с картинками, исправил некоторые недочеты по ревью наставника

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,15 @@
 'use strict';
 var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+var headerToggle = document.querySelector('.js-page-header-toggle');
+
+var modalTown = document.querySelector('.town');
+var townOpenButton = document.querySelector('.js-town-link');
+var townCloseButton = document.querySelector('.js-town-close-toggle');
+var blackout = document.querySelector('.blackout');
+
+var modalQuestion = document.querySelector('.question');
+var questionOpenButton = document.querySelector('.js-question-link');
+var questionCloseButton = document.querySelector('.js-question-close-toggle');
 
 headerToggle.addEventListener('click', function () {
   if (pageHeader.classList.contains('page-header--menu-closed')) {
@@ -12,19 +21,17 @@ headerToggle.addEventListener('click', function () {
   }
 });
 
-var modalTown = document.querySelector('.town');
-var townOpenButton = document.querySelector('.contacts__town-link');
-var townCloseButton = document.querySelector('.town__toggle');
-
 townOpenButton.addEventListener('click', function (evt) {
   evt.preventDefault();
   modalTown.classList.remove('town--hidden');
   modalTown.classList.add('town--visible');
+  blackout.classList.remove('blackout--none');
 });
 
 townCloseButton.addEventListener('click', function () {
   modalTown.classList.remove('town--visible');
   modalTown.classList.add('town--hidden');
+  blackout.classList.add('blackout--none');
 });
 
 window.addEventListener('keydown', function (evt) {
@@ -33,24 +40,22 @@ window.addEventListener('keydown', function (evt) {
     if (modalTown.classList.contains('town--visible')) {
       modalTown.classList.remove('town--visible');
       modalTown.classList.add('town--hidden');
+      blackout.classList.add('blackout--none');
     }
   }
 });
-
-
-var modalQuestion = document.querySelector('.question');
-var questionOpenButton = document.querySelector('.question-link');
-var questionCloseButton = document.querySelector('.question__toggle');
 
 questionOpenButton.addEventListener('click', function (evt) {
   evt.preventDefault();
   modalQuestion.classList.remove('question--hidden');
   modalQuestion.classList.add('question--visible');
+  blackout.classList.remove('blackout--none');
 });
 
 questionCloseButton.addEventListener('click', function () {
   modalQuestion.classList.remove('question--visible');
   modalQuestion.classList.add('question--hidden');
+  blackout.classList.add('blackout--none');
 });
 
 window.addEventListener('keydown', function (evt) {
@@ -59,6 +64,7 @@ window.addEventListener('keydown', function (evt) {
     if (modalQuestion.classList.contains('question--visible')) {
       modalQuestion.classList.remove('question--visible');
       modalQuestion.classList.add('question--hidden');
+      blackout.classList.add('blackout--none');
     }
   }
 });
