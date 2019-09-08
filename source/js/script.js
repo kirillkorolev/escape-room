ae7d0b6890886d9e96af099b33f1e082f480f8ff
Кирилл <kkirillcore@gmail.com>
2019-09-09 01:03:12 +0300

    правки

--- source/js/script.js
+++ source/js/script.js
@@ -1,138 +1,141 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.js-page-header-toggle');
 
-var modalTown = document.querySelector('.town');
-var townOpenButton = document.querySelector('.js-town-link');
-var townCloseButton = document.querySelector('.js-town-close-toggle');
-var blackout = document.querySelector('.blackout');
+(function () {
+  var pageHeader = document.querySelector('.page-header');
+  var headerToggle = document.querySelector('.js-page-header-toggle');
 
-var modalQuestion = document.querySelector('.question');
-var questionOpenButton = document.querySelector('.js-question-link');
-var questionCloseButton = document.querySelector('.js-question-close-toggle');
+  var modalTown = document.querySelector('.town');
+  var townOpenButton = document.querySelector('.js-town-link');
+  var townCloseButton = document.querySelector('.js-town-close-toggle');
+  var blackout = document.querySelector('.blackout');
 
-var questionOpenButtonMobile = document.querySelector(
-    '.js-question-mobile-link'
-);
+  var modalQuestion = document.querySelector('.question');
+  var questionOpenButton = document.querySelector('.js-question-link');
+  var questionCloseButton = document.querySelector('.js-question-close-toggle');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--menu-closed')) {
-    pageHeader.classList.remove('page-header--menu-closed');
-    pageHeader.classList.add('page-header--menu-opened');
-  } else {
-    pageHeader.classList.add('page-header--menu-closed');
-    pageHeader.classList.remove('page-header--menu-opened');
-  }
-});
+  var questionOpenButtonMobile = document.querySelector(
+      '.js-question-mobile-link'
+  );
 
-var openPopup = function (hiddenClass, visibleClass, openButton, popup) {
-  if (openButton) {
-    openButton.addEventListener('click', function (evt) {
-      evt.preventDefault();
-      popup.classList.remove(hiddenClass);
-      popup.classList.add(visibleClass);
-      blackout.classList.remove('blackout--none');
-    });
-  }
-};
+  headerToggle.addEventListener('click', function () {
+    if (pageHeader.classList.contains('page-header--menu-closed')) {
+      pageHeader.classList.remove('page-header--menu-closed');
+      pageHeader.classList.add('page-header--menu-opened');
+    } else {
+      pageHeader.classList.add('page-header--menu-closed');
+      pageHeader.classList.remove('page-header--menu-opened');
+    }
+  });
 
-var closePopup = function (hiddenClass, visibleClass, closeButton, popup) {
-  if (closeButton) {
-    closeButton.addEventListener('click', function () {
-      popup.classList.remove(visibleClass);
-      popup.classList.add(hiddenClass);
-      blackout.classList.add('blackout--none');
-    });
-  }
-};
+  var openPopup = function (hiddenClass, visibleClass, openButton, popup) {
+    if (openButton) {
+      openButton.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        popup.classList.remove(hiddenClass);
+        popup.classList.add(visibleClass);
+        blackout.classList.remove('blackout--none');
+      });
+    }
+  };
 
-openPopup('town--hidden', 'town--visible', townOpenButton, modalTown);
-openPopup(
-    'question--hidden',
-    'question--visible',
-    questionOpenButton,
-    modalQuestion
-);
-
-openPopup(
-    'question--hidden',
-    'question--visible',
-    questionOpenButtonMobile,
-    modalQuestion
-);
-
-closePopup('town--hidden', 'town--visible', townCloseButton, modalTown);
-closePopup(
-    'question--hidden',
-    'question--visible',
-    questionCloseButton,
-    modalQuestion
-);
-
-var closePopupWithEsc = function (hiddenClass, visibleClass, popup) {
-  window.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === 27) {
-      evt.preventDefault();
-      if (popup.classList.contains(visibleClass)) {
+  var closePopup = function (hiddenClass, visibleClass, closeButton, popup) {
+    if (closeButton) {
+      closeButton.addEventListener('click', function () {
         popup.classList.remove(visibleClass);
         popup.classList.add(hiddenClass);
         blackout.classList.add('blackout--none');
+      });
+    }
+  };
+
+  openPopup('town--hidden', 'town--visible', townOpenButton, modalTown);
+  openPopup(
+      'question--hidden',
+      'question--visible',
+      questionOpenButton,
+      modalQuestion
+  );
+
+  openPopup(
+      'question--hidden',
+      'question--visible',
+      questionOpenButtonMobile,
+      modalQuestion
+  );
+
+  closePopup('town--hidden', 'town--visible', townCloseButton, modalTown);
+  closePopup(
+      'question--hidden',
+      'question--visible',
+      questionCloseButton,
+      modalQuestion
+  );
+
+  var closePopupWithEsc = function (hiddenClass, visibleClass, popup) {
+    window.addEventListener('keydown', function (evt) {
+      if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (popup.classList.contains(visibleClass)) {
+          popup.classList.remove(visibleClass);
+          popup.classList.add(hiddenClass);
+          blackout.classList.add('blackout--none');
+        }
       }
+    });
+  };
+
+  closePopupWithEsc('town--hidden', 'town--visible', modalTown);
+  closePopupWithEsc('question--hidden', 'question--visible', modalQuestion);
+
+  blackout.addEventListener('click', function () {
+    blackout.classList.add('blackout--none');
+
+    if (modalTown.classList.contains('town--visible')) {
+      modalTown.classList.remove('town--visible');
+      modalTown.classList.add('town--hidden');
+    } else if (modalQuestion.classList.contains('question--visible')) {
+      modalQuestion.classList.remove('question--visible');
+      modalQuestion.classList.add('question--hidden');
     }
   });
-};
 
-closePopupWithEsc('town--hidden', 'town--visible', modalTown);
-closePopupWithEsc('question--hidden', 'question--visible', modalQuestion);
+  var inputName = modalQuestion.querySelector('.js-input-name');
+  var inputMail = modalQuestion.querySelector('.js-input-mail');
+  var errorMail = modalQuestion.querySelector('.form__mail-error');
 
-blackout.addEventListener('click', function () {
-  blackout.classList.add('blackout--none');
+  var nameInputTitle = modalQuestion.querySelector('.form__input-name-title');
+  var emailInputTitle = modalQuestion.querySelector('.form__input-email-title');
 
-  if (modalTown.classList.contains('town--visible')) {
-    modalTown.classList.remove('town--visible');
-    modalTown.classList.add('town--hidden');
-  } else if (modalQuestion.classList.contains('question--visible')) {
-    modalQuestion.classList.remove('question--visible');
-    modalQuestion.classList.add('question--hidden');
-  }
-});
+  inputMail.onblur = function () {
+    emailInputTitle.classList.add('form__input-name-title--visible');
+    if (!inputMail.value.includes('@')) {
+      inputMail.classList.add('form__error');
+      errorMail.innerHTML = 'Введён некорректный e-mail, попробуйте заново';
+    } else {
+      inputMail.classList.add('form__valid');
+    }
+  };
 
-var inputName = modalQuestion.querySelector('.js-input-name');
-var inputMail = modalQuestion.querySelector('.js-input-mail');
-var errorMail = modalQuestion.querySelector('.form__mail-error');
+  inputMail.onfocus = function () {
+    if (this.classList.contains('form__error')) {
+      this.classList.remove('form__error');
+      errorMail.innerHTML = '';
+      emailInputTitle.classList.remove('form__input-name-title--visible');
+    }
+  };
 
-var nameInputTitle = modalQuestion.querySelector('.form__input-name-title');
-var emailInputTitle = modalQuestion.querySelector('.form__input-email-title');
+  inputName.onblur = function () {
+    nameInputTitle.classList.add('form__input-name-title--visible');
+  };
 
-inputMail.onblur = function () {
-  emailInputTitle.classList.add('form__input-name-title--visible');
-  if (!inputMail.value.includes('@')) {
-    inputMail.classList.add('form__error');
-    errorMail.innerHTML = 'Введён некорректный e-mail, попробуйте заново';
-  } else {
-    inputMail.classList.add('form__valid');
-  }
-};
+  var checkboxForm = modalQuestion.querySelector('.js-form-checkbox');
+  var sendButton = modalQuestion.querySelector('.js-form-send');
 
-inputMail.onfocus = function () {
-  if (this.classList.contains('form__error')) {
-    this.classList.remove('form__error');
-    errorMail.innerHTML = '';
-    emailInputTitle.classList.remove('form__input-name-title--visible');
-  }
-};
-
-inputName.onblur = function () {
-  nameInputTitle.classList.add('form__input-name-title--visible');
-};
-
-var checkboxForm = modalQuestion.querySelector('.js-form-checkbox');
-var sendButton = modalQuestion.querySelector('.js-form-send');
-
-checkboxForm.addEventListener('change', function () {
-  if (checkboxForm.checked) {
-    sendButton.removeAttribute('disabled');
-  } else {
-    sendButton.setAttribute('disabled');
-  }
-});
+  checkboxForm.addEventListener('change', function () {
+    if (checkboxForm.checked) {
+      sendButton.removeAttribute('disabled');
+    } else {
+      sendButton.setAttribute('disabled');
+    }
+  });
+})();
